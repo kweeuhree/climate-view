92008b500829411dc19eda5962b455878747b845
Nika K <firstnamenika@gmail.com>
2024-05-23 10:50:36 -0400

    routes for user model

--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -4,7 +4,35 @@ const router = express.Router();
 const UserModel = require('../models/UserModel');
 const userController = require('../controllers/UserController');
 
+// id params
+router.param('id', async (req, res, next, id) => {
 
+    const found = await User.findById(id);
+    //if id exists, create a userId
+    if(found) {
+        req.userId = id;
+        next(); //call next function
+    } else { //if id doesn't exist send 404 status
+        res.status(404).send('User not found');
+    }
+});
+
+// get all users
+router.get('/', userController.fetchAllUsers);
+
+// get a specific user
+router.get('/:id', userController.getUser);
+
+// update a user
+router.get('/:id', userController.updateUser);
+
+// create a user
+router.get('/', userController.createUser);
+
+// delete a user
+router.get('/:id', userController.deleteUser);
+
+module.exports = router;
 
 
 
