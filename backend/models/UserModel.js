eec582c0b38cd417edcfdb93efd6ed4c74f85973
Nika K <firstnamenika@gmail.com>
2024-05-23 10:40:29 -0400

    created user and comment schemas

---
+++ backend/models/UserModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {type: String, require: true},
+    email: {type: String, require: true},
+    comments: [ {type: String} ]
+}, 
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
---
+++ backend/models/commentModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const CommentModel = new mongoose.Schema({
+    body: { type: String, requried: true },
+    postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, 
+    { timestamps: true }
+);
+
+const Comment = mongoose.model('Comment', CommentModel);
+
+module.exports = Comment;
