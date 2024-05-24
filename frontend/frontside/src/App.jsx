9e0ccc5e80acf3b1e5a131a51d37b3a9b839ae07
Nika K <firstnamenika@gmail.com>
2024-05-24 08:50:37 -0400

    added routes

--- frontend/frontside/src/App.jsx
+++ frontend/frontside/src/App.jsx
@@ -6,8 +6,11 @@ import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import UsefulLink from './components/UsefulLink';
-import { Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import CommentSection from './components/CommentSection';
+import HomePage from './pages/HomePage';
+import HistoryPage from './pages/HistoryPage';
+import ImpactPage from './pages/ImpactPage';
 
 function App() {
   const [weather, setWeather] = useState(null);
@@ -29,7 +32,10 @@ function App() {
       <NavBar loggedIn={loggedIn}/>
 
         <Routes>
-
+          <Route path="/" element={<HomePage />}/>
+          <Route path='/dashboard' element={<HomePage />} />
+          <Route path='/history' element={<HistoryPage />} />
+          <Route path='/impact' element={<ImpactPage />} />
         </Routes>
 
       <CommentSection loggedIn={loggedIn}/>
