2b7ecd166f91c68a6fbaa6bbdd5c040159f741fd
Nika K <firstnamenika@gmail.com>
2024-05-27 17:50:43 -0400

    history page styling

--- frontend/frontside/src/components/City/City.jsx
+++ frontend/frontside/src/components/City/City.jsx
@@ -3,15 +3,20 @@ import './CitiesStyle.css';
 
 const City = ({city, removeCity}) => {
   return (
-    <div className='city-container'>
+    <div className='display-flex flex-center city-container'>
       <div className="top-section">
           <div>{city.name}</div>
-          <div>{city.stateRegion}</div>
-          <div>{city.country}</div>
+          <div>
+            <div>{city.stateRegion}</div>
+            <div>{city.country}</div>
+          </div>
       </div>
 
-      <div>date: {city.date}</div>
-      <div>{city.temp}</div>
+      <div className='display-flex flex-center date-temp'>
+        <div>date: {city.date}</div>
+        <div className='font-size-2rem'>{city.temp}</div>
+      </div>
+
       <div className="big-button" onClick={()=>removeCity(city)}>✖</div>
     </div>
   )
