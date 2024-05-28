f0a283626b3a4c863aab117f664b0e8984ef1993
Nika K <firstnamenika@gmail.com>
2024-05-28 11:24:36 -0400

    added comments to CommentForm

--- frontend/frontside/src/components/CommentForm/CommentForm.jsx
+++ frontend/frontside/src/components/CommentForm/CommentForm.jsx
@@ -5,12 +5,14 @@ const CommentForm = ({user, addNewComment}) => {
 
   const userId = user.id;
 
+  // form data state for dynamic form change
   const [formData, setFormData] = useState({
     body: '',
     postedBy: userId,
     userName: user.name
   })
 
+  // handle form data change dynamically
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -18,22 +20,26 @@ const CommentForm = ({user, addNewComment}) => {
     })
   }
 
+  // handle form submit
   const handleSubmit = async (event) => {
+    // prevent default form behavior
     event.preventDefault();
     console.log(formData, ' formData inside handleSubmit of commentForm')
+
+    //attempt a post request
     try{
       const response = await fetch('http://localhost:3000/comments', {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
       });
-
+        //get comment object
         const data = await response.json();
         console.log(data, 'handle submit inside commentForm');
 
-        //update to trigger a render
+        // pass information into parent component
         addNewComment(data.comment);
-
+        //reset form data
         setFormData({body:'', postedBy: userId, userName: user.name});
     } catch (error) {
         console.log(error, 'error inside handle submit');
@@ -43,10 +49,12 @@ const CommentForm = ({user, addNewComment}) => {
   return (
     <div className='comment-container'>
 
+      {/* new form submit */}
         <form onSubmit={handleSubmit}>
           <textarea
             type='text'
             name='body'
+            // handle change dynamically
             onChange={handleChange}
             value={formData.body}
           />
