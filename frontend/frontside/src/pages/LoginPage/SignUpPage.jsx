de8c3e0b2cb3d61f38ee482377bb64c2a9632538
Nika K <firstnamenika@gmail.com>
2024-05-28 10:30:28 -0400

    added comments to signUpPage

--- frontend/frontside/src/pages/LoginPage/SignUpPage.jsx
+++ frontend/frontside/src/pages/LoginPage/SignUpPage.jsx
@@ -5,24 +5,31 @@ import Sphere from '../../components/Sphere';
 
 const SignUpPage = () => {
 
+    // trigger signup status message
     const [signUpMessage, setSignUpMessage] = useState(null);
+    // handle form data dynamically
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         password: ''
       })
-    const navigate = useNavigate();
 
-      function handleChange(event) {
-        setFormData({
-          ...formData,
-          [event.target.name]: event.target.value
-        })
+    const navigate = useNavigate(); //for redirection
+
+    // handle form data change dynamically
+    function handleChange(event) {
+      setFormData({
+        ...formData,
+        [event.target.name]: event.target.value
+      })
       }
     
-      const handleSubmit = async (event) => {
-        event.preventDefault();
-        console.log(formData);
+    //  handle sign up form submit
+    const handleSubmit = async (event) => {
+      event.preventDefault(); //prevent default form behavior
+      console.log(formData);
+
+      //  try signing up the user
         try{
           const response = await fetch('http://localhost:3000/auth/signup', {
             method: "POST",
@@ -36,34 +43,36 @@ const SignUpPage = () => {
 
             //update to trigger a render
             setSignUpMessage(true);
+            //reset form data
             setFormData({name:'', email: '', password: ''});
-            navigate('/login');
-           } else {
+            navigate('/login'); //redirect to login page
 
+           } else {
             console.log('failed to sign up');
-            setSignUpMessage(false);
+            setSignUpMessage(false); // trigger status message
            }
     
-            
-    
         } catch (error) {
-            console.log(error, 'errror inside handle submit');
+            console.log(error, 'error inside handle submit of SignUp');
         }
       }
 
   return (
     <div className='sign-up'>
+      {/* header and sphere */}
           <header>
             <h1>join the community 
              <Sphere />
               </h1>
             </header>
 
-           <form onSubmit={handleSubmit}>
+        {/* sign up form */}
+       <form onSubmit={handleSubmit}>
     
         <input 
           type='text' 
           name="name" 
+          // handle change dynamically
           onChange={handleChange} 
           placeholder='name'
           value={formData.name}
@@ -71,6 +80,7 @@ const SignUpPage = () => {
           <input 
              type='text' 
              name="email" 
+             // handle change dynamically
              onChange={handleChange} 
              placeholder='email'
              value={formData.email}
@@ -78,12 +88,15 @@ const SignUpPage = () => {
             <input 
                 type='text' 
                 name="password" 
+                // handle change dynamically
                 onChange={handleChange} 
                 placeholder='password'
                 value={formData.password}
             />
             <button type='submit'>Submit</button>
         </form>
+
+        {/* show sign up status message */}
         {signUpMessage === true && <div>sign up successful</div>}
         {signUpMessage === false && <div>failed sign up</div>}
   </div>
