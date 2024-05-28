a5421aafe7b1d9fd126ce4dc29b9888fceae6f07
Nika K <firstnamenika@gmail.com>
2024-05-28 11:21:36 -0400

    added comment to UsefulLink

--- frontend/frontside/src/components/UsefulLink/UsefulLink.jsx
+++ frontend/frontside/src/components/UsefulLink/UsefulLink.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './UsefulLink.css';
 
-
 const UsefulLink = () => {
     const fundraisers = 'https://www.facebook.com/fundraisers/explore/search/charities/?query=ukraine';
   
     return (
     <div className='fundraiser-container'>
+        {/* display the link, open in a new tab */}
         <a href={fundraisers} alt="Fundraisers for Ukraine" target='_blank' rel='noopener noreferrer'>
             Fundraisers for Ukraine: Get Involved
         </a>
