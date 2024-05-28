a829cf59f1d4ce97521e679b927b4925b4131d1b
Nika K <firstnamenika@gmail.com>
2024-05-28 11:20:21 -0400

    added comments to CommentSection

--- frontend/frontside/src/components/CommentSection/CommentSection.jsx
+++ frontend/frontside/src/components/CommentSection/CommentSection.jsx
@@ -6,31 +6,43 @@ import './CommentSection.css';
 
 const CommentSection = ({loggedIn, user}) => {
 
+  // set all comments state as empty array 
   const [comments, setComments] = useState([]);
 
+  // fetch comments on render
   useEffect(() => {
     const fetchComments = async () => {
+      // try fetching
       try {
         const fetchedComments = await getComments();
         // console.log('fetched comments inside try block ', fetchedComments);
-        setComments(fetchedComments.comments);
+        setComments(fetchedComments.comments); //set comments
         // console.log('fetchedComments.comments on the front after fetching ', fetchedComments.comments);
       } catch (error) {
+        //catch errors
         console.log('error inside fetchComments ', error);
       }
     }
 
+    //call the function
     fetchComments();
   
-  }, []);
+  }, []); //fetch once
 
+
+  //pass new comment to a parent function
   const addNewComment = (newComment) => {
     console.log(newComment);
+      // save new comment to state
     setComments((prevComments) => [...prevComments, newComment]);
   }
 
+
+  // handle edit inside parent component
   const handleEdit = async (event) => {
     console.log(event, ', event inside handleEdit of CommentSection');
+
+    // try requesting the update
     try {
       const response = await fetch(`http://localhost:3000/comments/${event._id}`, {
         method: 'PUT',
@@ -39,11 +51,12 @@ const CommentSection = ({loggedIn, user}) => {
       });
 
       if(response.ok) {
+        // get comment object
         const data = await response.json();
         console.log('updated comment inside handleEdit of commentSection: ', data.updatedComment);
-
+        //extract new comment
         const updatedComment = data.updatedComment;
-
+        //set state, replace old comment, match by id 
         setComments((prevComments) => 
           prevComments.map((comment) => 
             comment._id === event._id ? updatedComment : comment
@@ -56,6 +69,7 @@ const CommentSection = ({loggedIn, user}) => {
     }
   };
 
+  // handle delete of comment
   const handleDelete = async (id) => {
 
     //remove from database
@@ -78,22 +92,25 @@ const CommentSection = ({loggedIn, user}) => {
 
               {/* all comments */}
                 <ul className='comment-list'>
+                  {/* display comments if they exist */}
                     {comments.length > 0 ? (
                       comments.map((comment) => ( 
                         <Comment 
                           key={comment._id} 
                           user={user} 
                           comment={comment} 
+                          // pass handler functions
                           handleEdit={handleEdit} 
                           handleDelete={handleDelete}
                         />
                       ))
                     ) : (
+                      // display a message if there are no comments yet
                       <div className='no-access-message'>No comments yet</div>
                     )}
                   </ul>
 
-            
+            {/* if user is logged in show comment input box */}
                   {loggedIn ? (
                 <div className='all-comments-container'>
                   <CommentForm addNewComment={addNewComment} user={user} />
