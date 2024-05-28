2de801b4f912490745c2c0336b7b04a7df719769
Nika K <firstnamenika@gmail.com>
2024-05-28 13:13:54 -0400

    added comments to ImageSliderContainer

--- frontend/frontside/src/components/ImageSliderContainer/ImageSliderContainer.jsx
+++ frontend/frontside/src/components/ImageSliderContainer/ImageSliderContainer.jsx
@@ -4,6 +4,7 @@ import './ImageSliderContainer.css';
 
 const ImageSliderContainer = () => {
 
+  // isHovered state triggers appearance of image flags
     const [isHovered, setIsHovered] = useState(false);
 
     const flags = ['Radar Image', 'Thermal Image'];
@@ -12,9 +13,11 @@ const ImageSliderContainer = () => {
   return (
       
     <div className="image-slider-container"
+    // change state on mouse enter and mouse leave
       onMouseEnter={()=>setIsHovered(true)}
       onMouseLeave={()=>setIsHovered(false)}
       >
+        {/* dynamically apply style */}
       <div className={`flag-container left ${isHovered ? 'soft-appear' : ''}`}>
         <div className='flag'>{flags[0]}</div>
       </div>
@@ -23,7 +26,7 @@ const ImageSliderContainer = () => {
         <div className='flag'>{flags[1]}</div>
       </div>
 
-
+        {/* image container */}
           <ReactCompareImage
               className="slider"
               leftImage="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2024/05/iceberg_a-83_breaks_free/26094469-6-eng-GB/Iceberg_A-83_breaks_free_pillars.jpg"
