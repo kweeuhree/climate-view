742b35e31ac1f1a9f5ef458fe7c13de4739bef6f
Nika K <firstnamenika@gmail.com>
2024-05-29 11:43:15 -0400

    added more media for Atmospehere

--- frontend/frontside/src/components/Atmosphere/Atmosphere.jsx
+++ frontend/frontside/src/components/Atmosphere/Atmosphere.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import Video from './Video';
 import Header from '../Header';
+import './AtmosphereStyle.css';
+import image15 from '../../assets/atmosphereImages/image15.png';
+import image20 from '../../assets/atmosphereImages/image20.png';
+import image30 from '../../assets/atmosphereImages/image30.png';
+import image40 from '../../assets/atmosphereImages/image40.png';
 
 const Atmosphere = () => {
+
+    const descriptions = {
+        context: `Carbon dioxide in the atmosphere warms the planet, 
+        causing climate change.`
+    };
+    const images = [image15, image20, image30, image40];
+    const imagesJSX = images.map((item) => (<div className='image-parent'><img src={item} alt="Earth map with temperatures rising" /></div>));
+
   return (
     <article className='atmospehere-article'>
         <Header header={'atmosphere'} />
+        <div className="map-image-container">
+           <div className="all-images-parent">{imagesJSX}</div>
+            <div className="description">
+                {descriptions.context}
+            </div>
+        </div>
+
       <div className='video-container'>
         <Video />
         <div className='description video-description'>Clouds help to cool Earth. Further reading: <span>
--- frontend/frontside/src/components/Intro/Intro.jsx
+++ frontend/frontside/src/components/Intro/Intro.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import './Intro.css';
 
 const Intro = () => {
+
+  const descriptions = {
+    afterImage: ` Carbon dioxide emissions from fossil fuels reached another 
+    record high in 2023, according to a `,
+    linkText: `Global Carbon Project study.`
+    };
+
   return (
     <article className="intro-article">
         <div className="gif-div">
@@ -9,6 +16,16 @@ const Intro = () => {
                 src='https://media.licdn.com/dms/image/D5610AQEii3Qb0GPARg/image-shrink_800/0/1701811163703?e=1717549200&v=beta&t=CCQZGtUW90F28BFJj9ck8c0d2LJF2MRs0y_adEfelEM' 
                 alt="Earth surrounded by Carbon Monoxide gas" 
             />
+        </div>
+        <div className="description">
+        {descriptions.afterImage}
+            <span>
+              <a 
+                target="_blank" 
+                rel="noreferrer noopener"
+                href="https://essd.copernicus.org/articles/15/5301/2023/"
+                > {descriptions.linkText}</a>
+            </span>
         </div>
     </article>
   )
