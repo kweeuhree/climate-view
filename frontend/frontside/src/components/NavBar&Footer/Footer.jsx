ea9c5127693d20961e87ac9f5c74f92ab4aeb85d
Nika K <firstnamenika@gmail.com>
2024-05-29 22:36:33 -0400

    added comments to Footer and NavBar

--- frontend/frontside/src/components/NavBar&Footer/Footer.jsx
+++ frontend/frontside/src/components/NavBar&Footer/Footer.jsx
@@ -4,12 +4,14 @@ import './NavBar.css';
 
 const Footer = ({ loggedIn }) => {
 
+  // get current year
   const year = new Date().getFullYear();
 
   return (
     <footer>
+      {/* display navigation */}
       <NavBar loggedIn={loggedIn}/>
-
+      {/* display author name and current year */}
       <div className="copyright">© Veronika Kolesnikova, {year}</div>
     </footer>
   )
