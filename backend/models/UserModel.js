33685c03a46ed282d42d6f0e22d19887c006c60a
Nika K <firstnamenika@gmail.com>
2024-05-30 07:50:44 -0400

    comments for models

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -4,12 +4,13 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, require: true },
     email: { type: String, require: true },
     password: { type: String, require: true },
-        //populate all their comments----------------------------------------
     comments: [ { type: String } ]
 }, 
     { timestamps: true }
 );
 
+//create a model
 const User = mongoose.model('User', UserSchema);
 
+//export
 module.exports = User;
--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose');
 
 const CommentModel = new mongoose.Schema({
     body: { type: String, requried: true },
+    // require a user who posted the comment
     postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     userName: { type: String, requried: true }
 }, 
     { timestamps: true }
 );
 
+//create a model
 const Comment = mongoose.model('Comment', CommentModel);
 
+
+//export
 module.exports = Comment;
