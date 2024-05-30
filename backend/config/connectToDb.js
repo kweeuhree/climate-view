a434ca186bcfc734b8f77aa0b678a6a0c75f5f11
Nika K <firstnamenika@gmail.com>
2024-05-30 07:41:59 -0400

    comments for config.js

--- backend/config/connectToDb.js
+++ backend/config/connectToDb.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
-const mongoose  = require('mongoose');
+require('dotenv').config(); //require env to access enviroment variables
+const mongoose  = require('mongoose'); 
 
+//mongo db url
 const DB_URL = process.env.DB_URL;
 
+//connect to mongo
 const connectToDb = async () => {
     mongoose.connect(DB_URL);
     console.log('konnekted to kluster');
