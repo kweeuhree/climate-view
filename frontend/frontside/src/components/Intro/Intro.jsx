c0d28cf4567b3f8f7380a8855c25887aa5509f13
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-05 17:01:33 -0400

    changed intro gif

--- frontend/frontside/src/components/Intro/Intro.jsx
+++ frontend/frontside/src/components/Intro/Intro.jsx
@@ -60,7 +60,7 @@ const Intro = () => {
             {/* gif */}
             <div className="gif">
                     <img 
-                            src='https://media.licdn.com/dms/image/D5610AQEii3Qb0GPARg/image-shrink_800/0/1701811163703?e=1717549200&v=beta&t=CCQZGtUW90F28BFJj9ck8c0d2LJF2MRs0y_adEfelEM' 
+                            src='frontend\frontside\src\assets\carbon-earth.gif' 
                             alt="Earth surrounded by Carbon Monoxide gas" 
                         />
             </div>
