ecec77bd7e4fe4997815c2685c97638de187372a
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-10 19:37:50 -0400

    added comments to NavBar and NavIcon

--- frontend/frontside/src/components/NavBar&Footer/NavBar.jsx
+++ frontend/frontside/src/components/NavBar&Footer/NavBar.jsx
@@ -4,32 +4,37 @@ import './NavBar.css';
 import NavIcon from '../NavIcon/NavIcon';
 
 const NavBar = ({loggedIn, type}) => {
-  
+  //checks whether mobile sidebar is visible
   const [isVisible, setIsVisible] = useState(false);
-
+  //nav items names
   const links = ['dashboard', 'history', 'impact'];
 
+  // on click check if sidebar is visible, if it is - close it
   const handleLinkClick = () => {
     isVisible ? setIsVisible(prevState => !prevState) : null;
   }
 
+  // on click of burger icon change visibility of side bar
   const handleBurgerIconClick = () => {
     setIsVisible(prevState => !prevState);
   }
 
+  //generate list items for NavBar's unordered list
   const linksJSX = links.map((item, index) => (
     <li key={index}>
+      {/* navigate to relevant path and change visibility of sidebar */}
       <Link to={`/${item}`} onClick={handleLinkClick}>{item}</Link>
     </li>
   ))
 
+  // display profile path
   const profile = () => {
     return (
       <Link to="/profile">profile</Link>
     )
   }
-//if nav ul isnt visible show burger icon
 
+  // display sign up / log in paths
   const signlog = () => {
     return (
      <div className="signlog">
@@ -43,24 +48,30 @@ const NavBar = ({loggedIn, type}) => {
     )
   }
   
+  //main function---------------------------------
   return (
     
     <nav>
 
-     <NavIcon 
+      {/* NavIcon, display burger or home icons */}
+     <NavIcon
+      // pass icon type 
       type={type} 
       isVisible={isVisible} 
       handleBurgerIconClick={handleBurgerIconClick}
       />
 
+    {/* in mobile view, if mobile sidebar is visible display navigation list */}
       {isVisible && 
         <div className='overlay'>
           <ul style={{display: 'flex'}}>
+            {/* navigation list items */}
             {linksJSX}
           </ul>
         </div>
       }
 
+      {/* display navigation bar in non-mobile view */}
       <ul>{linksJSX}</ul>
 
       <div className='authcheck'>
--- frontend/frontside/src/components/NavIcon/NavIcon.jsx
+++ frontend/frontside/src/components/NavIcon/NavIcon.jsx
@@ -4,29 +4,37 @@ import './NavIconStyle.css';
 
 const NavIcon = ({type, isVisible, handleBurgerIconClick}) => {
 
+    // on click of to top button
     const handleClick = () => {
-        handleBurgerIconClick;
+        //scroll to top
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
     }
 
+    // display burger icon
     const burgerIcon = () => {
         return (
+            // on click of burger icon change visibility of sidebar
             <div className='burger-icon' onClick={handleBurgerIconClick}>
+                {/* if nav ul is visible show close icon */}
                 { isVisible ? (
                     <IoCloseOutline  className='close-icon'/>
                     ) : (
+                    // else show burger icon
                     <IoMenu  />
                 )}
             </div>
         );
     }
     
+    // display Home button
     const toTopIcon = () => {
         return (
+            //on click of Home button scroll to top
             <div onClick={handleClick}>
+                {/* Home button icon */}
                 <IoTriangleOutline />
             </div>
         )
